c7c35a673eb4b6cd10c10baae37cbcccae912840
tinacai1997 <tinacai1997@gmail.com>
2022-05-16 10:02:43 -0700

    Adding and Editing App Container Names

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
-import SignIn from './Containers/signIn';
-import Feed from './Containers/feed'
-import Onboarding from './Containers/onboarding';
-import EventNote from './Containers/eventNote';
+import Splash from './Containers/splash/splash';
+import Onboarding from './Containers/onboarding/onboarding';
+import Auth from './Containers/auth/auth';
+import Feed from './Containers/feed/feed'
+import Notes from './Containers/notes/notes';
 import { Routes, Route } from 'react-router-dom';
 
 
@@ -15,10 +16,11 @@ function App() {
   return (
     
       <Routes>
-        <Route exact path={'/'} element={<Onboarding />} />
-        <Route exact path={'/signin'} element={<SignIn />} />
+        <Route exact path={'/splash'} element={<Splash />} />
+        <Route exact path={'/onboarding'} element={<Onboarding />} />
+        <Route exact path={'/auth'} element={<Auth />} />
         <Route exact path={'/feed'} element={<Feed />}/>
-        <Route exact path={'/eventnote'} element={<EventNote />} />
+        <Route exact path={'/notes'} element={<Notes />} />
         
       </Routes>
     
