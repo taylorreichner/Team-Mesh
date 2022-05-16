cbbe40caac3a8e97b45992d25b90ccfeee56cba5
tinacai1997 <tinacai1997@gmail.com>
2022-05-16 10:01:04 -0700

    Renaming auth and adjusting auth style

---
+++ src/Components/signin-button.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import '../style.css'
+import '../Components/button.css'
+
+const SigninButton = () => {
+  const { loginWithRedirect } = useAuth0();
+  return (
+    <>
+    <div className='button button-secondary'
+      onClick={() => loginWithRedirect()}
+    >
+      <h4>Sign In</h4>
+    </div>
+    </>
+  );
+};
+
+export default SigninButton;
