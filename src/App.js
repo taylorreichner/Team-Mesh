a1468beaae4fca33150a26e258c7cbc8057f558a
tinacai1997 <tinacai1997@gmail.com>
2022-05-17 15:19:32 -0700

    Rearrange App.js routes
    Also add comments for test register and signin pages

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Splash from './Containers/splash-container/splash';
 import Onboarding from './Containers/onboarding-container/onboarding';
 import Auth from './Containers/auth-container/auth';
 import Explore from './Containers/feed-container/explore'
-import { Routes, Route } from 'react-router-dom';
 import DetailEmpty from './Containers/detail-container/detailEmpty';
 import DetailFilled from './Containers/detail-container/detailFilled';
 import Note from './Containers/notes/note'
+// import Register from './Containers/register'
+// import Signin from './Containers/signin'
 
 function App() {
   return (
-    
       <Routes>
-        <Route exact path={'/'} element={<Onboarding />} />
-        <Route exact path={'/:id'} element={<DetailEmpty />} />
-        <Route exact path={'/explore'} element={<Explore />}/>
-        <Route exact path={'/note'} element={<Note />} />
-        <Route exact path={'/note/:id'} element={<DetailFilled />} />
         <Route exact path={'/splash'} element={<Splash />} />
         <Route exact path={'/onboarding'} element={<Onboarding />} />
         <Route exact path={'/auth'} element={<Auth />} />
+        <Route exact path={'/explore'} element={<Explore />}/>
+        <Route exact path={'/:id'} element={<DetailEmpty />} />
+        <Route exact path={'/note/:id'} element={<DetailFilled />} />
+        <Route exact path={'/note'} element={<Note />} />
+        {/* <Route exact path={'/register'} element={<Register />} />
+        <Route exact path={'/signin'} element={<Signin />} /> */}
       </Routes>
-    
   );
 }
 
