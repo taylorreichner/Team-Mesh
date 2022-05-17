8c5659eda87adee26a7e03a14a34fefffd44b0d0
tinacai1997 <tinacai1997@gmail.com>
2022-05-17 15:18:37 -0700

    Style detail pages

--- src/Containers/detail-container/detailEmpty.jsx
+++ src/Containers/detail-container/detailEmpty.jsx
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { fetchEventsById } from '../../Services/eventsApi';
+import '../../style.css'
+import '../../Components/css/inputField.css'
+import '../detail-container/detail.css'
+
+import CloseIcon from '../../assets/icons/icon-close-21.svg'
 
 const DetailEmpty = () => {
   const [event, setEvent] = useState([]);
@@ -11,7 +16,6 @@ const DetailEmpty = () => {
   const [host, setHost] = useState('')
   const [note, setNote] = useState('')
   
-
   const createNewUserEvent = async (e) => {
       e.preventDefault()
       await fetch(`http://localhost:3001/api/v1/userevents/new`, {
@@ -23,59 +27,50 @@ const DetailEmpty = () => {
               date,
               host,
               note
-              
             })
-        })
-        
+        }) 
   }
-
- 
 
   useEffect(() => {
     
     fetchEventsById(id)
       .then(setEvent)
-    setTitle(event.title)
-    setUrl(event.url)
-    setDate(event.date)
-    setHost(event.host)
+      setTitle(event.title)
+      setUrl(event.url)
+      setDate(event.date)
+      setHost(event.host)
       
   }, [id, event.title, event.url, event.date, event.host]);
 
-  
   return (
     <div>
-    
-     <div className="event-card">
-         <div className="event-card-info">
-             <h3>{event.title}</h3>
-             <p className="grey">{event.host}</p>
-             <p className="grey">{event.date}</p>
-             <p className="grey">{event.url}</p>
-             
+      <img src={CloseIcon} alt="Close Icon" className="close-button" />
+      <h4 className="details-title">Event Notes</h4>
+      <div className="event-card details-card">
+        <div className="event-card-info">
+            <h3>{event.title}</h3>
+            <p className="grey">{event.host}</p>
+            <p className="grey">{event.date}</p>
+            <p className="grey">{event.url}</p>
+        </div>
+      </div>
 
-            <form onSubmit={createNewUserEvent}>
-        <span>
-          <textarea placeholder="I met Tina" onChange={(e) => setNote(e.target.value)}/>
-        </span>
-        
-          <span>
-            <button type="submit" variant="contained" size="large" color="primary">
-        Add Question; 
-            </button>
-          </span> 
-        
-      </form>
-
-
-
-         </div>
-         <div className="event-card-icons">
-             <div>hi</div>
-             <div>hi</div>
-         </div>
-     </div>
+    <form onSubmit={createNewUserEvent}>
+      {/* <span>
+        <textarea placeholder="Enter your event notes (Contacts, LinkedIn’s, etc.)" onChange={(e) => setNote(e.target.value)}/>
+      </span> */}
+      <div class="input-field-box details-notes-box">
+        <label className="input-box-label"><p>Event Notes</p></label>
+        <textarea placeholder="Enter your event notes (Contacts, LinkedIn’s, etc.)" onChange={(e) => setNote(e.target.value)}>
+        </textarea>
+      </div>
+      
+      <button className="button-primary" type="submit">
+        <h4>Save Notes</h4>
+      </button>
+    </form>
      
+    <div className="rect round-rect rect-blue rect-border"></div>
  </div>
   );
 };
--- src/Containers/detail-container/detailFilled.jsx
+++ src/Containers/detail-container/detailFilled.jsx
@@ -2,17 +2,16 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { fetchNoteEventsById } from '../../Services/eventsApi';
 
+import CloseIcon from '../../assets/icons/icon-close-21.svg'
+
 const DetailFilled = () => {
   const [event, setEvent] = useState([]);
   const { id } = useParams();
   
-  
-
   useEffect(() => {
     
     fetchNoteEventsById(id)
       .then(setEvent)
-    
       
   }, [id]);
 
@@ -20,24 +19,19 @@ const DetailFilled = () => {
   
   return (
     <div>
-    
-     <div className="event-card">
-         <div className="event-card-info">
-             <h3>{event.title}</h3>
-             <p className="grey">{event.host}</p>
-             <p className="grey">{event.date}</p>
-             <p className="grey">{event.url}</p>
-             <div>{event.note}</div>
+      <img src={CloseIcon} alt="Close Icon" className="close-button" />
+      <h4 className="details-title">Event Notes</h4>
+      <div className="event-card details-card">
+          <div className="event-card-info">
+              <h3>{event.title}</h3>
+              <p className="grey">{event.host}</p>
+              <p className="grey">{event.date}</p>
+              <p className="grey">{event.url}</p>
+              <div>{event.note}</div>
+          </div>
+      </div>
 
-
-
-         </div>
-         <div className="event-card-icons">
-             <div>hi</div>
-             <div>hi</div>
-         </div>
-     </div>
-     
+     <div className="rect round-rect rect-blue rect-border"></div>
  </div>
   );
 };
