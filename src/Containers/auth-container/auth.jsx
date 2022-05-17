89a3082e89b0364205b36fc1f8118665dfa3d639
tinacai1997 <tinacai1997@gmail.com>
2022-05-17 15:18:09 -0700

    Edit auth for removal of auth0

--- src/Containers/auth-container/auth.jsx
+++ src/Containers/auth-container/auth.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import AuthNav from '../../Components/auth-nav';
+// import AuthNav from '../../Components/auth-nav';
 import './auth.css'
-import Logo from '../../assets/icon-logo-54.png'
+import '../../Components/css/backButton.css'
 
-import AuthImg from '../../assets/auth-welcome.png'
+import Logo from '../../assets/icons/icon-logo-54.png'
+import AuthImg from '../../assets/auth/auth-welcome.png'
+import BackButton from '../../assets/icons/icon-back-24.svg'
 
 const Auth = () => {
     return (
         <>
+        <img src={BackButton} alt="Back Button" className='back-button' />
         <div className="auth-description">
             <img src={AuthImg} alt="Welcome" className="auth-img" />
             <div className="auth-title">
@@ -16,12 +19,14 @@ const Auth = () => {
             </div>
             <p className='grey'>A place for developers, designers, and product managers to gain tech career traction</p>
         </div>
-        <div className="register-button">
-        </div>
-        <div className="signin-button">
-            <AuthNav />
-        </div>
-        <div className="round-rect rect-white rect-flip auth-rect"> </div>
+        <button className="button-primary button-1">
+            <h4>Register</h4>
+        </button>
+        <button className="button-secondary button-2">
+            <h4>Sign In</h4>
+            {/* <AuthNav /> */}
+        </button>
+        <div className="rect round-rect rect-white rect-flip auth-rect"> </div>
         <div className="background"></div>
         </> 
     )
